cb99ae21152fd3e3e457cf3fc946d4f287eb4e3f
dvvcobr <50865630+dvvcobr@users.noreply.github.com>
2021-09-14 16:25:32 +0500

    Update вычисление корней квадратного уравнения.cpp

--- вычисление корней квадратного уравнения.cpp
+++ вычисление корней квадратного уравнения.cpp
@@ -3,6 +3,7 @@
 #include <cmath>
 
 using namespace std;
+function float diskriminant(int a,b,c);
 
 int main(){
   setlocale(LC_ALL,"Russian");
@@ -10,8 +11,8 @@ int main(){
 
   cout << "Введите a b и c " << endl;
   cin >> a>>b>>c;
-
-  float d=pow(b,2)-4*a*c;
+  float d=diskriminant(a,b,c);
+  //float d=pow(b,2)-4*a*c;
 
   if(d>0){
     cout<<"1) "<<(-b+sqrt(d))/(2*a);
@@ -22,4 +23,8 @@ int main(){
     cout<<"корней нет.";
 
   return 0;
+}
+
+function float diskriminant(int a,b,c){
+  return(pow(b,2)-4*a*c);
 }
