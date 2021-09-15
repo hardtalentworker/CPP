65b0decae0c33cab30f7773e99ea544f12df7c2a
dvvcobr <50865630+dvvcobr@users.noreply.github.com>
2021-09-15 17:24:00 +0500

    Update простой калькулятор.cpp

--- простой калькулятор.cpp
+++ простой калькулятор.cpp
@@ -6,8 +6,7 @@ using namespace std;
 
 int main(){
   setlocale(LC_ALL,"Russian");
-  int a=0,b=0;
-  float result=0;
+  float a=0,b=0,result=0;
   char c;
 
   cout<<"Введите первый операнд: ";
@@ -20,24 +19,28 @@ int main(){
   switch(c){
     case '+':
       result=a+b;
+      cout<<"Результат: "<<result;
       break;
     case '-'  :
       result=a-b;
+      cout<<"Результат: "<<result;
       break;
     case '*'  :
       result=a*b;
+      cout<<"Результат: "<<result;
       break;
     case '/'  :
-      if(b!=0)
+      if(b!=0){
         result=a/b;
+        cout<<"Результат: "<<result;
+      }
       else
         cout<<"Деление на 0";
       break;
-    default
+    default:
       cout<<"Неверная операция";
       break;
   }
-  cout<<"Результат: "<<result;
 
   return 0;
 }
