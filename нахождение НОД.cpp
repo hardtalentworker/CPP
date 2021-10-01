80ed67f1fc153a242cc2068924f19072d3b7029b
dvvcobr <50865630+dvvcobr@users.noreply.github.com>
2021-10-01 14:33:12 +0500

    Update нахождение НОД.cpp

--- нахождение НОД.cpp
+++ нахождение НОД.cpp
@@ -6,43 +6,24 @@ using namespace std;
 int main()
 {
   setlocale(LC_ALL,"Russian");
-  int pp=0,a=0,b=0,result=0,minimal=0;
+  int a=0,b=0,nod=0,i=0;
 
-  cout<<"Поиск наименьшего общего кратного(НОК)"<<endl;
-  cout<<"Выберите способ поиска:"<<endl;
-  cout<<"1 - перебор до 1000 вариантов"<<endl;
-  cout<<"2 - поиск через НОД"<<endl;
-  cin>>pp;
+  cout<<"Поиск наибольшего общего делителя(НОД)"<<endl;
   cout<<"Введите два числа"<<endl;
-      cin>>a>>b;
-      a=(a<0?-a:a);
-      b=(b<0?-b:b);
-  int nod=0,i=0;
-  switch(pp){
-    case 1:
-      minimal=(a<b?a:b);
+  cin>>a>>b;
+  a=(a<0?-a:a);
+  b=(b<0?-b:b);
 
-      for(int i=1;i<=a*b;i++){
-        if(((minimal*i)%a==0)&&((minimal*i)%b==0)&&(result==0))
-          result=minimal*i;
-      }
-      break;
-    case 2:
-      while(i<=((a<b)?a:b)){
-      i++;
-      if((b%i==0)&&(a%i==0))
-        nod=i;
-      }
-      result=a*b/nod;
-      break;
-    default:
-      cout<<"Выбран неверный пункт - "<<pp<<endl;
-      break;
+  while(i<=((a<b)?a:b)){
+    i++;
+    if((b%i==0)&&(a%i==0))
+      nod=i;
   }
-    if(result==0)
-        cout<<"НОК для "<<a<<" и "<<b<<" не найден"<<endl;
-      else
-        cout<<"НОК для "<<a<<" и "<<b<<" = "<<result<<endl;
+
+    if(nod==0)
+      cout<<"НОК для "<<a<<" и "<<b<<" не найден"<<endl;
+    else
+      cout<<"НОК для "<<a<<" и "<<b<<" = "<<nod<<endl;
 
 	return 0;
 }
